b7ea3b58430c90e3712ae89110d65763bb1bac62
danaremar <56022332+danaremar@users.noreply.github.com>
2023-01-14 11:54:15 +0100

    Added errors in login view
    Added errors when login

--- src/app/errors/errors.component.ts
+++ src/app/errors/errors.component.ts
@@ -1,11 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Match } from "../models/match";
 import { PrincipalComponent } from "../principal.component";
 
 @Component({
-    selector: 'errors',
+    selector: 'errors-footmatch',
     templateUrl: './errors.component.html',
     styleUrls: ['./errors.component.scss']
 })
-export class ErrorsComponent extends PrincipalComponent {
+export class ErrorsComponent implements OnChanges{
+    
+
+    @Input()
+    containError: boolean = false
+
+    @Input()
+    messageError: string = ''
+
+    ngOnChanges() {}
 
 }
