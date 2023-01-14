bfc6bffde84cc9edcc0de648b9a91ad4554dd9a2
danaremar <56022332+danaremar@users.noreply.github.com>
2023-01-14 11:12:54 +0100

    Added component for error handling

---
+++ src/app/errors/errors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { PrincipalComponent } from "../principal.component";
+
+@Component({
+    selector: 'errors',
+    templateUrl: './errors.component.html',
+    styleUrls: ['./errors.component.scss']
+})
+export class ErrorsComponent extends PrincipalComponent {
+
+}
