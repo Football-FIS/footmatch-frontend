4d1b0a47fedd167e96f1c6d16007764569ea237b
Victor <vmvg29@gmail.com>
2023-01-19 00:31:24 +0100

    Cacheado

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -5,56 +5,46 @@ import { MatchStatus } from '../models/matchStatus';
 import { PrincipalComponent } from '../principal.component';
 import { MatchService } from '../services/match.service';
 import { MatchStatusService } from '../services/matchStatus.service';
-
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
   styleUrls: ['./match.component.scss']
 })
 export class MatchComponent extends PrincipalComponent implements OnInit {
-
   url: string = ''
   match: Match | null | undefined
   matchStatus: MatchStatus | null | undefined
   matchStatusList: MatchStatus[] = [];
   matchStatusList4update: MatchStatus[] = [];
-
   constructor(private route: ActivatedRoute, private matchService: MatchService, private matchStatusService: MatchStatusService) {
     super();
   }
-
   ngOnInit() {
-
     // set url
     this.route.params.subscribe(params => {
       this.url = params['url'];
       console.log('URL is: ' + this.url)
     });
-
+    this.matchStatusService.getMatchStatusInCache(this.url)
     // get match information
     this.matchService.getMatchByUrl(this.url).subscribe({
       next: (n) => {
         this.containError = false
-        //crear funcion para insertar lista de eventos
         this.match = n
+        this.loadMyStatus();
       },
       error: (e) => {
         this.returnPrincipalError(e)
       }
     })
-
-    this.loadMyStatus();
-
   }
-
   loadMyStatus(){
-
     // get match status information
     this.matchStatusService.getMatchStatusByMatchId(this.url).subscribe({
       next: (n) => {
         this.containError = false
+        this.matchStatusService.setMatchStatusesInCache(this.url, n)
         this.matchStatusList = n
-        
         // now, sort the list to bring us the newest status
         this.matchStatusList.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
         this.matchStatus = this.matchStatusList[0];
@@ -64,16 +54,11 @@ export class MatchComponent extends PrincipalComponent implements OnInit {
         this.returnPrincipalError(e)
       }
     })
-
   }
-
-
   getLatestMatchStatus(): MatchStatus[] {
     return this.matchStatusList.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).slice(-3);
   }
-
   openModal() {
     this.show_modal = true
   }
-
 }
--- src/app/services/matchStatus.service.ts
+++ src/app/services/matchStatus.service.ts
@@ -5,44 +5,38 @@ import { environment } from "src/environments/environment";
 import { MatchStatus } from "../models/matchStatus";
 import { TokenService } from "../team-service/token.service";
 import { FormControl, FormGroup } from '@angular/forms';
-
+import { Match } from "../models/match";
+const SELECTED_MATCH = 'SelectedMatch'
+const MATCH_STATUS_LIST = 'MatchStatusList'
 @Injectable({
     providedIn: 'root'
 })
 export class MatchStatusService {
-
     // URL
     matchStatusUrl = environment.match_status_serv_url + '/api/v1/match_status/'
-
     // CONSTRUCTOR
     constructor(private httpClient: HttpClient, private tokenService: TokenService) { }
-
     /*
     *
     * OPERATIONS ACTIVES
     * 
     */
-
     // GET
     public getMatchStatusById(matchStatusId: string): Observable<any> {
         let url = this.matchStatusUrl + matchStatusId
         return this.httpClient.get<any>(url)
     }
-
     // GET By MATCHID 
     public getMatchStatusByMatchId(matchId: string): Observable<any> {
         let url = this.matchStatusUrl + 'matchid/' + matchId
         return this.httpClient.get<any>(url)
     }
-
     // CREATE
     public createMatchStatus(matchStatus: MatchStatus): Observable<any> {
         let url = this.matchStatusUrl
         return this.httpClient.post<any>(url, matchStatus)
     }
-
     // POST TEMPORAL
-
     public postMatchStatus(form:FormGroup): Observable<any> {
       let httpOptions = {
         headers: new HttpHeaders({
@@ -53,58 +47,31 @@ export class MatchStatusService {
       let url = this.matchStatusUrl
       return this.httpClient.post<any>(url, form.value, httpOptions)
   }
-
     // UPLOAD
     public updateMatchStatus(matchStatus: MatchStatus): Observable<any> {
         let url = this.matchStatusUrl
         return this.httpClient.put<any>(url, matchStatus)
     }
-
     // DELETE
     public deleteMatchStatus(matchStatusId: string): Observable<any> {
         let url = this.matchStatusUrl + matchStatusId
         return this.httpClient.delete<any>(url)
     }
-
-
+    /*
+    *
+    * LOCALSTORAGE CACHE
+    * 
+    */
+    public getMatchStatusInCache(matchUrl: any): Array<MatchStatus> {
+        let ls = localStorage.getItem(MATCH_STATUS_LIST)
+        let m = localStorage.getItem(SELECTED_MATCH)
+        if(ls!=null && m==matchUrl) {
+            return JSON.parse(ls)
+        }
+        return []
+    }
+    public setMatchStatusesInCache(matchUrl: any, myMatches: Array<MatchStatus>): void {
+        localStorage.setItem(SELECTED_MATCH, matchUrl)
+        localStorage.setItem(MATCH_STATUS_LIST, JSON.stringify(myMatches))
+    }
 }
-/*
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-const API_URL = 'http://localhost:8000/api/';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class StatusService {
-
-  constructor(private http: HttpClient) { }
-
-  // Obtener un estado
-  getStatus(id: number): Observable<any> {
-    return this.http.get(API_URL + 'status/' + id);
-  }
-
-  // Crear un estado
-  createStatus(status: Object): Observable<Object> {
-    return this.http.post(API_URL + 'status/', status);
-  }
-
-  // Editar un estado
-  updateStatus(id: number, value: any): Observable<Object> {
-    return this.http.put(API_URL + 'status/' + id, value);
-  }
-
-  // Eliminar un estado
-  deleteStatus(id: number): Observable<any> {
-    return this.http.delete(API_URL + 'status/' + id, { responseType: 'text' });
-  }
-
-  // Listar estados
-  getStatusesList(): Observable<any> {
-    return this.http.get(API_URL + 'status/');
-  }
-}
-*/
